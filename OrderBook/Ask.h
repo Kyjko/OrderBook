f48584d70808af3a19f57245aa0108f94cd70677
Kyjko <bognarmiklos34@gmail.com>
2021-04-06 21:14:41 +0200

    operator overloading for print

--- OrderBook/Ask.h
+++ OrderBook/Ask.h
@@ -1,9 +1,15 @@
 #pragma once
+#include <ostream>
 class Ask {
 public:
 	Ask(const size_t size, const double value);
-	//Ask();
+
 	size_t size;
 	double value;
+	
+	friend std::ostream& operator<<(std::ostream& os, const Ask& ask);
 };
 
+std::ostream& operator<<(std::ostream& os, const Ask& ask) {
+	std::cout << ask.size << " " << ask.value;
+}
--- OrderBook/Bid.h
+++ OrderBook/Bid.h
@@ -5,5 +5,11 @@ public:
 	//Bid();
 	size_t size;
 	double value;
+
+	friend std::ostream& operator<<(std::ostream& os, const Bid& ask);
 };
+
+std::ostream& operator<<(std::ostream& os, const Bid& bid) {
+	std::cout << bid.size << " " << bid.value;
+}
 
